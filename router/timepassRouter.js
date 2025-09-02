ca1d497f685389d30ffdd6604333d0db291d82e1
Mitali Sinha <107989068+mira713@users.noreply.github.com>
2025-09-02 18:14:54 +0530

    Add files via upload

--- router/timepassRouter.js
+++ router/timepassRouter.js
@@ -37,11 +37,37 @@ timepassRouter.patch("/update/:id", async (req, res) => {
   const dish = req.body.dish;
   const price = req.body.price;
   const img = req.body.img;
+  const quantity = req.body.quantity;
   try {
-    await TimepassModel.findByIdAndUpdate(id, { dish, price, img });
+    await TimepassModel.findByIdAndUpdate(id, { dish, price, img , quantity});
     res.send({ message: "Item Updated" });
   } catch (e) {
     res.send(e.message);
+  }
+});
+
+// Add/Remove to wishlist for a specific user
+timepassRouter.patch("/wishlist/:id", async (req, res) => {
+  const { id } = req.params; // dish id
+  const { user } = req.body; // user id (from frontend or token)
+
+  try {
+    const dish = await TimepassModel.findById(id);
+    if (!dish) return res.status(404).send({ error: "Dish not found" });
+
+    // Prevent duplicate entries
+    if (!dish.user.includes(user)) {
+      dish.user.push(user);
+      await dish.save();
+    }else{
+      dish.user = dish.user.filter(u => u !== user);
+      await dish.save();
+      return res.send({ message: "Removed from wishlist", dish });
+    }
+
+    res.send({ message: "Added to wishlist", dish });
+  } catch (e) {
+    res.status(500).send({ error: e.message });
   }
 });
 
