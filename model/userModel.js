1ae9d2f4ca59dab8dbd6f49447845a9933510b0b
Mitali Sinha <107989068+mira713@users.noreply.github.com>
2025-10-15 12:27:46 +0530

    nodemailer for forgotPassword added

--- model/userModel.js
+++ model/userModel.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const UserSchema = mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  resetToken: { type: String },
+  resetTokenExpiry: { type: Number },
   },
   {
     versionKey: false,
