93e8f7489ac5c6d78d3d255ab5276fb7f1e139da
Mitali Sinha <107989068+mira713@users.noreply.github.com>
2025-10-27 17:16:23 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { authenticate } = require("./middleware/authenticate");
 const { userRouter } = require("./router/userRouter");
 const { todoRouter } = require("./router/todoRouter");
 const { timepassRouter } = require("./router/timepassRouter");
+const port = process.env.PORT || 8000;
 
 const app = express();
 
@@ -17,16 +18,16 @@ app.get("/", (req, res) => {
 });
 
 app.use("/users", userRouter);
-app.use(authenticate)
+app.use(authenticate);
 app.use("/todo", todoRouter);
-app.use("/timepass", timepassRouter)
+app.use("/timepass", timepassRouter);
 
-app.listen(process.env.PORT, async () => {
+app.listen(port, async () => {
   try {
     await db;
     console.log("Connected to DB");
   } catch (e) {
     console.log(e.message);
   }
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${port}`);
 });
